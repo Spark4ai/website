900ccca6314580f46fc099674e1cbfbf75785b9f
smartraunak <raunak5690@gmail.com>
2023-10-17 07:36:58 +0530

    socket

--- Spark4AI/settings.py
+++ Spark4AI/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import socketserver
+from wsgiref import handlers
+socketserver.BaseServer.handle_error = lambda *args, **kwargs: None
+handlers.BaseHandler.log_exception = lambda *args, **kwargs: None
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
